72d505b020753dd4733e3f1f7dc411952f03f245
Arham444 <ar7337198@gmail.com>
2026-06-02 21:54:39 +0500

    Implemented product detail page with dynamic routing and wishlist functionality.

--- src/features/products/productSelectors.js
+++ src/features/products/productSelectors.js
@@ -1,1 +1,8 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const selectAllProducts = (state) => state.products.items;
+
+export const selectProductById = (productId) =>
+  createSelector([(state) => state.products.items], (items) =>
+    items.find((item) => item.id === productId),
+  );
