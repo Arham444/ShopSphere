3a9ac10a0d2a051c5b51408130cea1d135ac42b0
Arham444 <ar7337198@gmail.com>
2026-06-09 00:52:57 +0500

    Fixed main layout padding and flex behavior across pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const location = useLocation();
-  const isLoginPage = location.pathname === "/login";
-
   return (
     <>
       <Navbar />
-      <main style={{ padding: isLoginPage ? "0" : "1rem" }}>
+      <main>
         <Outlet />
       </main>
     </>
