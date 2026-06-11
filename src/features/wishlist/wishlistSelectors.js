13efd7fa80e5181099420fb650f4a9d23ba5373e
Arham444 <ar7337198@gmail.com>
2026-06-11 19:43:18 +0500

    corrected  selectIsInWishlist to return boolean using Array.some() instead of find()

--- src/features/wishlist/wishlistSelectors.js
+++ src/features/wishlist/wishlistSelectors.js
@@ -3,7 +3,7 @@ import { createSelector } from "@reduxjs/toolkit";
 export const selectWishlistItems = (state) => state.wishlist.wishListItems;
 
 export const selectIsInWishlist = (state, productId) =>
-  state.wishlist.wishListItems.find((item) => item.id === productId);
+  state.wishlist.wishListItems.some((item) => item.id === productId);
 
 export const selectWishlistItemsCount = createSelector(
   selectWishlistItems,
